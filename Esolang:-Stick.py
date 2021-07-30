a3b71b64c1f7f25e212a5fd47734c273bc925217
Ganesh Patil <68324277+gproxx-coder@users.noreply.github.com>
2021-07-30 15:02:08 +0530

    Create  Esolang:-Stick.py

---
+++ Esolang:-Stick.py
@@ -0,0 +1,42 @@
+# https://www.codewars.com/kata/58855acc9e1de22dff0000ef/train/python
+
+def interpreter(tape):
+    check_point = 0
+    build = ""
+    stack = [0,]
+    idx = 0
+    while idx < len(tape):
+        if tape[idx] == '+':
+            if stack[-1] == 255:
+                stack[-1] = 0
+            else:
+                stack[-1] += 1
+        
+        elif tape[idx] == '-':
+            if stack[-1] == 0:
+                stack[-1] = 255
+            else:
+                stack[-1] -= 1
+        
+        elif tape[idx] == '*':
+            build += chr(stack[-1])
+            char_sum = 0
+        
+        elif tape[idx] == '^':
+            stack.pop()
+            char_sum -= 1
+            
+        elif tape[idx] == '!':
+            stack.append(0)
+            char_sum = 0
+            
+        elif tape[idx] == '[' and stack[-1] == 0:
+            check_point = idx
+            idx = idx+2
+            continue
+            
+        elif tape[idx] == ']' and stack[-1] != 0:
+            idx = check_point
+            continue
+        idx += 1
+    return build[:len(stack)]
